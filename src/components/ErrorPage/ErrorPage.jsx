bb0b3eac41e2e968c4efafc5ec6e33d40645336c
Mokarama <mokaramaaktershanta@gmail.com>
2025-03-26 21:00:39 +0600

    Refactor Navbar, Dashboard, HomePage, and Footer components

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">Page not Found</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Root from './components/Root/Root.jsx';
+import ErrorPage from './components/ErrorPage/ErrorPage.jsx';
+import HomePage from './components/HomePage/HomePage.jsx';
+import Dashbord from './components/Dashbord/Dashbord.jsx';
 let router = createBrowserRouter([
   {
     path: "/",
-    element: <div><h1>Hello world!</h1></div>,
+    element: <Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
+    children:[
+      {
+        path:'/',
+        element:<HomePage></HomePage>
+      },
+      {
+        path:'dashboard',
+        element:<Dashbord></Dashbord>
+      }
+    ]
     
   },
 ]);
